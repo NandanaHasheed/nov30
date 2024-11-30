d7572b24e3f2e854e1ce6f509b4d238372f440f3
NandanaHasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2024-11-30 10:26:43 +0530

    Add files via upload

---
+++ up.py
@@ -0,0 +1,7 @@
+str="The quick brow fox"
+str1=str.lower()
+str2=str.upper()
+print(f"The given string is in lower case : {str1}")
+print(f"The given string is in upper case : {str2}")
+print(f"The given string is lower : {str1.islower()}")
+print(f"The given string is upper :{str2.isupper()}")
