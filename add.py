047e0ee32ec456a42addb4c2d410cdee5809c229
NandanaHasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2024-11-30 09:14:47 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,11 @@
+'''
+Author: Nandana Hasheed
+Date:19-11-2024
+python program to add two numbers using function
+'''
+def add_two_numbers():
+    number_1=int(input("Enter number 1"))
+    number_2=int(input("Enter number 2"))
+    sum=number_1+number_2
+    print("Sum of two numbers = ",sum)
+add_two_numbers()
